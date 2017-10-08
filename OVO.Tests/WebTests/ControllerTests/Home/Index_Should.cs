aec2419fba44bfa841889111ab9bef244392e298
jorosoft <jorosoftbg@gmail.com>
2017-10-09 00:58:39 +0300

    Add simple unit test to config CI tool

---
+++ OVO.Tests/WebTests/ControllerTests/Home/Index_Should.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OVO.Web.Controllers;
+using System.Web.Mvc;
+
+namespace OVO.Tests.WebTests.ControllerTests.Home
+{
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void ReturnNotNull_WhenCalled()
+        {
+            // Arrange
+            var sut = new HomeController();
+
+            // Act
+            var result = sut.Index() as ActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
