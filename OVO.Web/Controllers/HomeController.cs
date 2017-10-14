ce2c4784cc29454ebe8a63fb823277adac19612d
jorosoft <jorosoftbg@gmail.com>
2017-10-14 16:06:16 +0300

    Refactor UI theme

--- OVO.Web/Controllers/HomeController.cs
+++ OVO.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace OVO.Web.Controllers
 {
@@ -11,19 +7,10 @@ namespace OVO.Web.Controllers
         public ActionResult Index()
         {
             return this.View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return this.View();
-        }
+        }        
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return this.View();
         }
     }
